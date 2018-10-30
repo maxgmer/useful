d641e60b2a35375f1fcb0f2aad23f5ff46caed8e
maksim.gapeenko <maksim.gapeenko@zimad.com>
2018-10-30 13:28:21 +0300

    Graph markup finished, a bit of refactoring done

--- lib/customWidgets/StatsGraph.dart
+++ lib/customWidgets/StatsGraph.dart
@@ -67,27 +67,16 @@ class _StatsGraphPainter extends CustomPainter {
         ..style = PaintingStyle.fill;
 
     paint.color = _graphMarkupColor;
-    //draw horizontal lines
+    //draw horizontal markup lines and values
     double fifthOfHeight = (size.height - (_HEIGHT_SPACE_FOR_TEXT * 2 /*2 because for bottom and top*/)) / 5;
-    for (int i = 0; i <= 5; i++) {
+    for (int i = 0, graphValueMarkupBase = 5; i <= 5; i++, graphValueMarkupBase--) {
       canvas.drawLine(Offset(StatsGraphValues.SPACE_FOR_NUMBERS_SIZE, _HEIGHT_SPACE_FOR_TEXT + (i * fifthOfHeight)),
           Offset(size.width, _HEIGHT_SPACE_FOR_TEXT + (i * fifthOfHeight)), paint);
 
-      //move this to a separate method
-      TextSpan span = TextSpan(
-          style: TextStyle(
-              color: Colors.black,
-              fontSize: StatsGraphValues.FONT_SIZE_VERTICAL_MARKUP_TEXT,
-              fontFamily: "Mali"
-          ),
-          text: (i * fifthOfGraphValueHeight).toString());
-      TextPainter activitiesAmount = TextPainter(text: span, textDirection: TextDirection.ltr);
-      activitiesAmount.layout();
-      activitiesAmount.paint(canvas, Offset(0.0, 10.0 + (i * fifthOfHeight)));
+      _drawText(canvas, (graphValueMarkupBase * fifthOfGraphValueHeight).toString(), Offset(0.0, 10.0 + (i * fifthOfHeight)));
     }
 
-
-    //draw vertical lines
+    //draw vertical markup lines and values
     double shiftToRight = timeFrame == StatsGraphTimeFrame.YEAR ? 0.9 : 1.25;
     double shiftToRightText = timeFrame == StatsGraphTimeFrame.YEAR ? 0.70 : 1.0;
     List<String> dayNames = StatsGraphTimeFrameHelper.getStrings(timeFrame);
@@ -96,22 +85,29 @@ class _StatsGraphPainter extends CustomPainter {
       canvas.drawLine(Offset(StatsGraphValues.SPACE_FOR_NUMBERS_SIZE * shiftToRight + (i * partOfWidth), _HEIGHT_SPACE_FOR_TEXT),
           Offset(StatsGraphValues.SPACE_FOR_NUMBERS_SIZE * shiftToRight + (i * partOfWidth), size.height - _HEIGHT_SPACE_FOR_TEXT), paint);
 
-      TextSpan span = TextSpan(
-          style: TextStyle(
-              color: Colors.black,
-              fontSize: StatsGraphValues.FONT_SIZE_HORIZONTAL_MARKUP_TEXT,
-              fontFamily: "Mali"
-          ),
-          text: dayNames[i]);
-      TextPainter activitiesAmount = TextPainter(text: span, textDirection: TextDirection.ltr);
-      activitiesAmount.layout();
-      activitiesAmount.paint(canvas, Offset(StatsGraphValues.SPACE_FOR_NUMBERS_SIZE * shiftToRightText + (i * partOfWidth),
+      _drawText(canvas, dayNames[i], Offset(StatsGraphValues.SPACE_FOR_NUMBERS_SIZE * shiftToRightText + (i * partOfWidth),
           size.height - StatsGraphValues.SPACE_FOR_NUMBERS_SIZE / 2));
-      }
+    }
+
+    //draw graph data
+
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return false;
+  }
+
+  void _drawText(Canvas canvas, String text, Offset position) {
+    TextSpan span = TextSpan(
+        style: TextStyle(
+            color: Colors.black,
+            fontSize: StatsGraphValues.FONT_SIZE_VERTICAL_MARKUP_TEXT,
+            fontFamily: "Mali"
+        ),
+        text: (text));
+    TextPainter activitiesAmount = TextPainter(text: span, textDirection: TextDirection.ltr);
+    activitiesAmount.layout();
+    activitiesAmount.paint(canvas, position);
   }
 }
--- lib/models/SessionDataModel.dart
+++ lib/models/SessionDataModel.dart
@@ -3,7 +3,7 @@ import 'package:useful_app/models/Activity.dart';
 class SessionDataModel {
   SessionDataModel();
 
-  List<Activity> _activities = [Activity("Hello"),Activity("Hello"),Activity("Hello"),Activity("Hello"),Activity("Hello")];
+  List<Activity> _activities = [Activity("Hello"), Activity("Hello"), Activity("Hello"), Activity("Hello"), Activity("Hello")];
   List<Activity> get activities => _activities;
   set activities(List<Activity> activities) => _activities = activities;
 
@@ -34,34 +34,34 @@ class StatsGraphTimeFrameHelper {
         DateTime beginningOfTheWeek = DateTime.now();
         beginningOfTheWeek = beginningOfTheWeek.toLocal();
         //reset time to 00:00
-        beginningOfTheWeek.subtract(
+        beginningOfTheWeek = beginningOfTheWeek.subtract(
             Duration(hours: beginningOfTheWeek.hour, minutes: beginningOfTheWeek.minute, seconds: beginningOfTheWeek.second));
         //reset weekday to monday
-        beginningOfTheWeek.subtract(
+        beginningOfTheWeek = beginningOfTheWeek.subtract(
             Duration(days: beginningOfTheWeek.weekday - 1));
         return beginningOfTheWeek;
       case StatsGraphTimeFrame.MONTH:
         DateTime beginningOfTheMonth = DateTime.now();
         beginningOfTheMonth = beginningOfTheMonth.toLocal();
         //reset time to 00:00
-        beginningOfTheMonth.subtract(
+        beginningOfTheMonth = beginningOfTheMonth.subtract(
             Duration(hours: beginningOfTheMonth.hour, minutes: beginningOfTheMonth.minute, seconds: beginningOfTheMonth.second));
         //reset day to first of the month
-        beginningOfTheMonth.subtract(
+        beginningOfTheMonth = beginningOfTheMonth.subtract(
             Duration(days: beginningOfTheMonth.day - 1));
         return beginningOfTheMonth;
       case StatsGraphTimeFrame.YEAR:
         DateTime beginningOfTheYear = DateTime.now();
         beginningOfTheYear = beginningOfTheYear.toLocal();
         //reset time to 00:00
-        beginningOfTheYear.subtract(
+        beginningOfTheYear = beginningOfTheYear.subtract(
             Duration(hours: beginningOfTheYear.hour, minutes: beginningOfTheYear.minute, seconds: beginningOfTheYear.second));
         //reset month to january
         while (beginningOfTheYear.month != 1)
-          beginningOfTheYear.subtract(
+          beginningOfTheYear = beginningOfTheYear.subtract(
               Duration(days: 30));
         //reset day to first of the month
-        beginningOfTheYear.subtract(
+        beginningOfTheYear = beginningOfTheYear.subtract(
             Duration(days: beginningOfTheYear.day - 1));
         return beginningOfTheYear;
     }
@@ -71,9 +71,9 @@ class StatsGraphTimeFrameHelper {
   //e.g for year timeframe returns number of activities in the best month (month with highest number of completed activities)
   static int getBestActivitiesNumber(StatsGraphTimeFrame timeFrame,
       List<Activity> currentTimeFrameActivities, DateTime beginningOfTheTimeFrame) {
-    int maxActivitiesInSegment = 0;
     switch(timeFrame) {
       case StatsGraphTimeFrame.WEEK:
+        int maxActivitiesInSegment = 0;
         while (true) {
           int activitiesInSegment = 0;
           for (Activity activity in currentTimeFrameActivities) {
@@ -89,10 +89,11 @@ class StatsGraphTimeFrameHelper {
           beginningOfTheTimeFrame = beginningOfTheTimeFrame.add(
               Duration(days: 1));
 
-          if (beginningOfTheTimeFrame.weekday == 7) break;
+          if (beginningOfTheTimeFrame.weekday == 7) return maxActivitiesInSegment;
         }
         break;
       case StatsGraphTimeFrame.MONTH:
+        int maxActivitiesInSegment = 0;
         //when we specify 0 for day it gives us last day of the previous month
         var lastDayOfMonth = DateTime(beginningOfTheTimeFrame.year, beginningOfTheTimeFrame.month + 1, 0);
         while (true) {
@@ -110,10 +111,11 @@ class StatsGraphTimeFrameHelper {
           beginningOfTheTimeFrame = beginningOfTheTimeFrame.add(
               Duration(days: 1));
 
-          if (beginningOfTheTimeFrame.day == lastDayOfMonth.day) break;
+          if (beginningOfTheTimeFrame.day == lastDayOfMonth.day) return maxActivitiesInSegment;
         }
         break;
       case StatsGraphTimeFrame.YEAR:
+        int maxActivitiesInSegment = 0;
         while (true) {
           //when we specify 0 for day it gives us last day of the previous month
           var lastDayOfMonth = beginningOfTheTimeFrame.month == 12 ?
@@ -134,11 +136,10 @@ class StatsGraphTimeFrameHelper {
           beginningOfTheTimeFrame = beginningOfTheTimeFrame.add(
               Duration(days: 1));
 
-          if (beginningOfTheTimeFrame.month == 12) break;
+          if (beginningOfTheTimeFrame.month == 12) return maxActivitiesInSegment;
         }
-        break;
     }
-    return maxActivitiesInSegment;
+    throw "Such timeframe does not exist";
   }
 }
 
